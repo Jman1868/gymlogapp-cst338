b6c63e4c353d8e12b3aab896bbaf554764b4e229
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2024-04-03 18:27:43 -0700

    Completed Video 6
    Able to see all the gymlogs by descending value

--- app/src/main/java/com/example/gymlog/database/GymLogDAO.java
+++ app/src/main/java/com/example/gymlog/database/GymLogDAO.java
@@ -22,7 +22,7 @@ public interface GymLogDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(GymLog gymLog);
 
-    @Query("Select * from " + GymLogDatabase.GYM_LOG_TABLE)
+    @Query("SELECT * FROM " + GymLogDatabase.GYM_LOG_TABLE +" ORDER BY date DESC")
     List<GymLog> getAllRecords();
 
 }
