a28a72ae482700dd12be73e513bb238aedc8a2be
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2024-04-04 04:52:22 -0700

    Completed video 11
    get by username broken

--- app/src/main/java/com/example/gymlog/database/UserDAO.java
+++ app/src/main/java/com/example/gymlog/database/UserDAO.java
@@ -13,6 +13,8 @@ import java.util.List;
 @Dao
 public interface UserDAO {
 
+
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(User... user);
 
@@ -25,4 +27,7 @@ public interface UserDAO {
 
     @Query("DELETE FROM " + GymLogDatabase.USER_TABLE )
     void deleteAll();
+
+    @Query("SELECT * FROM " + GymLogDatabase.USER_TABLE + " WHERE username == :username ")
+    User getUserByUserName(String username);
 }
