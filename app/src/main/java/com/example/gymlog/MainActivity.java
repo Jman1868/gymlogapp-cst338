790c80576b4fce0ec5461872e11b85a7754022e7
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2024-04-03 22:26:34 -0700

    Completed video 9
    Added a login screen
    
    incomplete build

--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gymlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.gymlog.MAIN_ACTIVITY_USER_ID";
     private ActivityMainBinding binding;
 
     private GymLogRepository repository;
@@ -35,9 +38,17 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        //Login stuff
+        loginUser();
+
+        if(loggedInUserId==-1){
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+
+
         repository = GymLogRepository.getRepository(getApplication());
-
-
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
         updateDisplay();
         binding.logButton.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +69,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //TODO: CREATE LOGIN METHOD
+    private void loginUser() {
+
+        loggedInUserId=getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID,-1);
+
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId){
+
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID,userId);
+        return intent;
+
+    }
 
     private void insertGymlogRecord(){
 
