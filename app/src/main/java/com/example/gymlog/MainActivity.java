bed73a2964152dfe03afef493c6ba7892ba29064
Jamal Ahmed <43161576+Jman1868@users.noreply.github.com>
2024-04-03 17:39:37 -0700

    Completed Video 5
    Works

--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        repository = new GymLogRepository(getApplication());
+        repository = GymLogRepository.getRepository(getApplication());
 
 
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         String currentInfo = binding.logDisplayTextView.getText().toString();
         String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=-=-=%n%s",mExercise,mWeight,mReps,currentInfo);
         binding.logDisplayTextView.setText(newDisplay);
+        Log.i(TAG,repository.getAllLogs().toString());
     }
 
 
